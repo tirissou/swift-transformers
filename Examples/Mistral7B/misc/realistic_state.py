b1e7ddc4fa5180a5e0c9b84e6c705dd4ce7f6c05
Thibault Irissou <irissou.thibault@gmail.com>
2024-11-04 22:32:13 -0800

    functional circular state!!!!!

--- Examples/Mistral7B/misc/realistic_state.py
+++ Examples/Mistral7B/misc/realistic_state.py
@@ -3,28 +3,45 @@ import coremltools as ct
 from typing import List
 import numpy as np
 from torch._C import dtype
+from torch.functional import Tensor
 
 class DynamicSizeArange(torch.nn.Module):
     """
     A basic usage of cond based on dynamic shape predicate.
     """
 
-    def __init__(self, size=8):
+    def __init__(self, size=8, max_sequence_length=2048):
         super().__init__()
+        self.size = size
+        self.max_sequence_length = max_sequence_length
         self.register_buffer('counter', torch.tensor([0], dtype=torch.half))
-        self.register_buffer('cache', torch.zeros(1024, 1, 2048, 1, 128, dtype=torch.half))
+        self.register_buffer('cache', torch.zeros(1024, 1, self.max_sequence_length, 1, 128, dtype=torch.half))
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        # for i in range(1024):
-            # self.cache[i] = 
-        # self.cache[:, :, :1, :, :] = x
-        # self.cache += 0
-        start = self.counter.int()
-        end = start + 1
+        index: Tensor = torch.remainder(self.counter, self.max_sequence_length).int()
+        # print(f"{index=}")
+        self.cache[:, :, index[0], :, :] = x
         self.counter += 1
-        # cache = self.cache[0]
-        self.cache[0, :, start:end, :, :] = x
-        return self.cache[0, :, 0:end, :, :]
+        start = torch.where(self.counter > self.max_sequence_length, index + 1, 0)
+        index = torch.max(torch.concat((start, torch.tensor([1]))))
+        # print(f"{start=}")
+        # print(f"{index=}")
+        # return self.cache + 0
+
+        rval = torch.where(
+            start == 0,
+            self.cache[0, :, :],
+            torch.concat((self.cache[0, :, index:], self.cache[0, :, :index],), dim=1)
+        )
+
+        rval = torch.einsum(
+            'abcd,adce->abce', rval, rval.transpose(1,3)
+        )
+
+        rval = torch.einsum(
+            'abcd,adce->abce', rval, rval.transpose(1,3)
+        )
+        return rval
 
 
 
@@ -32,10 +49,11 @@ class DynamicSizeArange(torch.nn.Module):
 
 
 # test_input = torch.arange(4, dtype=torch.half)
-test_input = torch.randn(1, 1, 1, 128, dtype=torch.half)
+test_input = torch.rand(1, 1, 1, 128, dtype=torch.half) * 0.25
 
 buffer_size = 8
-torch_model = DynamicSizeArange(buffer_size)
+max_sequence_length = 2048
+torch_model = DynamicSizeArange(buffer_size, max_sequence_length)
 query_length = ct.RangeDim(lower_bound=1, upper_bound=32, default=1)
 inputs: List[ct.TensorType] = [
     ct.TensorType(shape=ct.Shape(shape=(1, 1, 1, 128)), name="input_array", dtype=np.half)
@@ -43,7 +61,7 @@ inputs: List[ct.TensorType] = [
 
 states: List[ct.StateType] = [
     ct.StateType(
-        wrapped_type=ct.TensorType(shape=(1024, 1, 2048, 1, 128), dtype=np.half),
+        wrapped_type=ct.TensorType(shape=(1024, 1, max_sequence_length, 1, 128), dtype=np.half),
         name="cache",
     ),
     ct.StateType(
@@ -68,10 +86,26 @@ mlmodel_fp16 = ct.convert(
         debug=True,
         compute_precision=ct.transform.FP16ComputePrecision(),
     )
+
 mlmodel_fp16.save("realistic_basic_state_seq_dim1.mlpackage")
 
 state = mlmodel_fp16.make_state()
 
 print(mlmodel_fp16.predict({"input_array": test_input.numpy()}, state))
+
+
+
+print("Quantizing to int4")
+op_config = ct.optimize.coreml.OpLinearQuantizerConfig(                                     
+    mode="linear_symmetric",
+    dtype="int4",                                                                           
+    granularity="per_block",
+    block_size=32,                                                                          
+)
+config = ct.optimize.coreml.OptimizationConfig(global_config=op_config)
+mlmodel_int4 = ct.optimize.coreml.linear_quantize_weights(mlmodel_fp16, config=config)
+# mlmodel_int4._spec.description.metadata.userDefined.update({METADATA_TOKENIZER: MODEL_ID})
+# mlmodel_int4.optimization_hints['reshapeFrequency'] = ct.ReshapeFrequency.Frequent
+mlmodel_int4.save("realistic_int4.mlpackage")
 
 
